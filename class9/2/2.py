228a63a85639c5006289e447485d4122d62f1e08
rauoid <rlind987@gmail.com>
2019-06-26 14:24:43 -0700

    ex2

--- class9/2/2.py
+++ class9/2/2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-# from napalm import get_network_driver
 from pprint import pprint
 from my_devices import cisco3, arista1
-from my_functions import open_napalm_connection
+from my_functions import open_napalm_connection, create_backup
 
 
 my_device_conn_objects = []
@@ -13,19 +12,23 @@ my_device_conn_objects.append(my_cisco3_device)
 my_arista1_device = open_napalm_connection(arista1)
 my_device_conn_objects.append(my_arista1_device)
 
-
-# print(my_device_conn_objects)
 for my_device_conn_object in my_device_conn_objects:
+# 2a
    print("Napalm connection object: ")
    print(my_device_conn_object)
    print()
-# open connection
    my_device_conn_object.open()
 # 2b get arp table
    print("Get arp table: ")
    pprint(my_device_conn_object.get_arp_table())
    print()
-#
-#   print("Napalm platform: ")
-#   print(my_device_conn_object.platform)
-#   print()
+# 2c get ntp peers
+   print("Get NTP peers: ")
+   try:
+      print("NTP peers: {}".format(my_device_conn_object.get_ntp_peers()))
+   except NotImplementedError:
+      print("Command not implemented")
+      print()
+# 2d create backup
+   create_backup(my_device_conn_object)
+   print()
